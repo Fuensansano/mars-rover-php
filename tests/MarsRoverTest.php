6962266a974cf2927b938da626d8482f0314baae
Fuen Sansano <fuensansano@gmail.com>
2024-01-15 18:54:16 +0100

    REFACTOR: created setup method

--- tests/MarsRoverTest.php
+++ tests/MarsRoverTest.php
@@ -8,23 +8,23 @@ use PHPUnit\Framework\TestCase;
 
 class MarsRoverTest extends TestCase
 {
+    private $marsRover;
+
+    public function setUp(): void
+    {
+        $this->marsRover = new MarsRover();
+    }
+
+
     /** @test */
     public function given_a_M_command_then_rovers_should_be_in_0_1_N_position(): void
     {
-        $marsRover = new MarsRover();
-
-        $marsRover->execute("M");
-
-        self::assertEquals($marsRover->execute("M"), "0:1:N");
+        self::assertEquals($this->marsRover->execute("M"), "0:1:N");
     }
 
     /** @test */
     public function given_a_MM_command_then_rovers_should_be_in_0_1_N_position(): void
     {
-        $marsRover = new MarsRover();
-
-        $marsRover->execute("M");
-
-        self::assertEquals($marsRover->execute("MM"), "0:2:N");
+        self::assertEquals($this->marsRover->execute("MM"), "0:2:N");
     }
 }
