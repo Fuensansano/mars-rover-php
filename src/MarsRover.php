a32955548b61c1ea25fc12c8987816c510eaffd5
Fuen Sansano <fuensansano@gmail.com>
2024-01-15 19:29:51 +0100

    REFACTOR: production and test code

--- src/MarsRover.php
+++ src/MarsRover.php
@@ -5,10 +5,19 @@ namespace PhpKataSetup;
 
 class MarsRover
 {
+        public int $coordinate_x = 0;
+        public int $coordinate_y = 0;
     public function execute(string $string): string
     {
-        if ($string === "MMMMMMMMMM") return "0:0:N";
-        if ($string === 'MM') return "0:2:N";
-        return "0:1:N";
+
+
+        if (strlen($string) >= 10) return $this->coordinate_x . ":" . $this->coordinate_y . ":" . "N";
+
+        for ($i = 0; $i < strlen($string); $i++) {
+           $this->coordinate_y += 1;
+        }
+
+        return $this->coordinate_x . ":" . $this->coordinate_y . ":" . "N";
+
     }
 }
--- tests/MarsRoverTest.php
+++ tests/MarsRoverTest.php
@@ -19,18 +19,18 @@ class MarsRoverTest extends TestCase
     /** @test */
     public function given_a_M_command_then_rovers_should_be_in_0_1_N_position(): void
     {
-        self::assertEquals($this->marsRover->execute("M"), "0:1:N");
+        self::assertEquals("0:1:N", $this->marsRover->execute("M"));
     }
 
     /** @test */
     public function given_a_MM_command_then_rovers_should_be_in_0_1_N_position(): void
     {
-        self::assertEquals($this->marsRover->execute("MM"), "0:2:N");
+        self::assertEquals("0:2:N", $this->marsRover->execute("MM"));
     }
 
     /** @test */
     public function given_a_10_M_command_then_rovers_should_be_in_0_0_N_position(): void
     {
-        self::assertEquals($this->marsRover->execute("MMMMMMMMMM"), "0:0:N");
+        self::assertEquals("0:0:N", $this->marsRover->execute("MMMMMMMMMM"));
     }
 }
