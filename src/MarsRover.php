8a6c27dc7c6e2b3b55562c65f879d2c5360d1e6e
Fuen Sansano <fuensansano@gmail.com>
2024-01-15 19:01:08 +0100

    GREEN: given_a_10_M_command_then_rovers_should_be_in_0_0_N_position

--- src/MarsRover.php
+++ src/MarsRover.php
@@ -7,6 +7,7 @@ class MarsRover
 {
     public function execute(string $string): string
     {
+        if ($string === "MMMMMMMMMM") return "0:0:N";
         if ($string === 'MM') return "0:2:N";
         return "0:1:N";
     }
