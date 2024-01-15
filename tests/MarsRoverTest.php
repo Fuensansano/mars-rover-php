a6e6aa7c10d87abcad6d34a05641efeb1f888fda
Fuen Sansano <fuensansano@gmail.com>
2024-01-15 18:45:35 +0100

    RED: given_a_M_command_then_rovers_should_be_in_0_1_N_position

---
+++ tests/MarsRoverTest.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests;
+
+use PhpKataSetup\MarsRover;
+use PHPUnit\Framework\TestCase;
+
+class MarsRoverTest extends TestCase
+{
+    /** @test */
+    public function given_a_M_command_then_rovers_should_be_in_0_1_N_position(): void
+    {
+        $marsRover = new MarsRover();
+
+        $marsRover->execute("M");
+
+        assertEquals($marsRover->execute("M"), "0:1:N");
+    }
+}
