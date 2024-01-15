acd4598a31be2cf6848ae4e0fc729f068316d61e
Fuen Sansano <fuensansano@gmail.com>
2024-01-15 21:08:11 +0100

    REFACTOR: production code

--- src/MarsRover.php
+++ src/MarsRover.php
@@ -9,18 +9,22 @@ class MarsRover
         public int $coordinate_y = 0;
     public function execute(string $string): string
     {
+        $orientation =  [
+            1 => "E",
+            2 => "S",
+            3 => "W",
+            4 => "N",
+        ];
 
-        if ($string === 'R')  return $this->coordinate_x . ":" . $this->coordinate_y . ":" . "E";
-        if ($string === 'RR')  return $this->coordinate_x . ":" . $this->coordinate_y . ":" . "S";
-        if ($string === 'RRR')  return $this->coordinate_x . ":" . $this->coordinate_y . ":" . "W";
+        if (substr_count($string, "R"))  return $this->coordinate_x . ":" . $this->coordinate_y . ":" . $orientation[strlen($string)];
 
-        if (strlen($string) >= 10) return $this->coordinate_x . ":" . $this->coordinate_y . ":" . "N";
-
-        for ($i = 0; $i < strlen($string); $i++) {
-           $this->coordinate_y += 1;
+        if (strlen($string) >= 10) {
+            return $this->coordinate_x . ":" . $this->coordinate_y . ":" . "N";
+        } else {
+            for ($i = 0; $i < strlen($string); $i++) {
+                $this->coordinate_y += 1;
+            }
+            return $this->coordinate_x . ":" . $this->coordinate_y . ":" . "N";
         }
-
-        return $this->coordinate_x . ":" . $this->coordinate_y . ":" . "N";
-
     }
 }
